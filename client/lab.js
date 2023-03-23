c8764efecd29b555ff74e09d4d16bc2b1a784d10
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-23 14:59:19 -0600

    Adds rollbar connection

--- client/lab.js
+++ client/lab.js
@@ -5,4 +5,5 @@ button.addEventListener("click", saveInput)
 
 function saveInput(){
     alert(`I would like ${input.value}`)
+    input.textContent = ""
 }
--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(cors());
 
 app.use(express.static(`${__dirname}/client`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '35994f3a99784ba785820cececfeab5a',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/lab.html')
 // })
